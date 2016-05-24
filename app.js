dd218974beeb41ec3326120040b7693f72e00ab1
alex-latham <latham2017@gmail.com>
2016-05-23 21:17:47 -0400

    test
    test

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-var app = angular.module("extraNo", []);
+var app = angular.module("angularTest", []);
 
-app.controller("extraNo", function($scope) {
-  $scope.listEntry = "";
-  $scope.reallyNo = "dsgnkfjgof";
-  $scope.array = [1,2,3,4,5];
+app.controller("angularTest", function($scope) {
+  $scope.listEntry = ""; // empty string
+  $scope.array = [1,2,3,4,5]; // array of stuff, could be numbers, strings anything
   $scope.addToArray = function() {
     $scope.array.push($scope.listEntry);
   }
 
-  $scope.pizzaToppings = [
+  $scope.pizzaToppings = [ // data attribute array
     {"name":"cheese", "tastiness":"Heck Yeah"},
     {"name":"tuna", "tastiness":"90%"},
-    {"name":"pepperoni", "tastiness":"Heck No"},
-    {"name":"olives", "tastiness":"leave the world"},
+    {"name":"pepperoni", "tastiness":"maybe??"},
+    {"name":"olives", "tastiness":"why"},
     {"name":"crabs", "tastiness":"why is this a thing"},
     {"name":"hamburger", "tastiness":"what"}
   ];
